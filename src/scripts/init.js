555236804f315222cca02fb9d208410508dc0aa3
BEOM NAMGOONG <tombeom@naver.com>
2023-07-02 21:12:57 +0900

    Add Comment

--- src/scripts/init.js
+++ src/scripts/init.js
@@ -1,14 +1,16 @@
+// index.html 로드 시 위경도 좌표를 받아오고 실행에 필요한 함수를 순차적으로 실행시키는 javascript
+
+/**
+ * 위경도 좌표를 받아온 이후 실행에 필요한 함수들을 순차적으로 실행시키는 함수 - init.js
+ */
 function init(position) {
   setCoordinate(position);
-  setAddress();
   convertCoordToGridCoord();
   convertCoordToTMCoord();
   getNowcast();
-  setTimeout(() => {
-    drawPM();
-  }, '1500');
 }
 
+// 현재 위경도 좌표를 받아오고 init() - init.js 실행
 navigator.geolocation.getCurrentPosition(
   init,
   getPositionError,
@@ -18,15 +20,3 @@ navigator.geolocation.getCurrentPosition(
     maximumAge: 0,
   })
 );
-
-// $refresh.addEventListener("click", () => {
-//   navigator.geolocation.getCurrentPosition(
-//     init,
-//     getPositionError,
-//     (options = {
-//       enableHighAccuracy: true,
-//       timeout: 5000,
-//       maximumAge: 0,
-//     })
-//   );
-// });
